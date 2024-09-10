b83b8c156e9575d5ae280ed43383d0b63bff296e
RU063 <rashedinislam.06@gmail.com>
2024-09-10 17:27:03 +0600

    successfully setup firebase

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,98 @@
+import PropTypes from 'prop-types';
+import { createContext, useEffect, useState } from 'react';
+import {
+  createUserWithEmailAndPassword,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile
+} from 'firebase/auth';
+import auth from '../firebase/firebase.config';
+
+
+
+export const AuthContext = createContext(null);
+
+// auth Provider
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // set a observer
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser)
+      console.log(currentUser)
+      setLoading(false)
+    })
+    
+    return () => unSubscribe();
+  },[])
+
+  // create user
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password)
+  };
+
+  // update user
+  const updateUserProfile = ( name, photo) => {
+    setLoading(true)
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo
+    })
+  }
+
+  // login user
+  const logInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // google login
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // github login
+  const githubLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, githubProvider);
+  };
+
+  // sign out
+  const logOutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    updateUserProfile,
+    logInUser,
+    logOutUser,
+    googleLogin,
+    githubLogin,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export default AuthProvider;
