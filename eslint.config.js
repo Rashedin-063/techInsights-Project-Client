a504f03aebbffce70f1a34800c4b2e8c87fab33e
RU063 <rashedinislam.06@gmail.com>
2024-09-10 15:08:30 +0600

    tailwind css and daisyUI added

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,7 @@ export default [
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      node: true,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
